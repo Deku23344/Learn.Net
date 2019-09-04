76c71f0a160b7c1c6d0e2d83e184fc4f10e13168
Deku23344 <kurokosan9@gmail.com>
2019-09-04 22:46:25 +0300

    added new project

---
+++ Learn.Net/Learn.Net/Program.cs
@@ -0,0 +1,21 @@
+﻿using Learn.Net.srs.debug;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new DebugTest().HelloWorld("Red");
+            Console.ReadLine(); 
+
+
+        }
+    }
+}
